41ad72e3ecb24704d28f1e52668968c9ca05f27e
Vladimir Romanov <vladromweb@gmail.com>
2016-01-12 20:43:24 -0500

    Servo Sweep based on Potentiometer
    Servo Sweep based on Potentiometer

--- ArduinoPotentiometerServo/ArduinoPotentiometerServo.ino
+++ ArduinoPotentiometerServo/ArduinoPotentiometerServo.ino
@@ -1,9 +1,23 @@
+#include <Servo.h>
+Servo test;
+int AnalogOut = 0;
+int NewAnalogOut = 0;
+
 void setup() {
   Serial.begin(9600);
+  test.attach(11);
 }
 // test
 void loop() {
   int sensorValue = analogRead(A0);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  Serial.println(voltage);
+  
+  NewAnalogOut = map(sensorValue, 0, 1023, 0, 180);
+  Serial.print(AnalogOut);
+  Serial.println(NewAnalogOut);
+  if (abs(NewAnalogOut - AnalogOut) > 2) {
+    test.write(AnalogOut);
+    AnalogOut = NewAnalogOut;
+  }
+  delay(1);
+  
 }
