d60a7772e070541fa726928966f4dded7c6ecb43
Vladimir Romanov <vladromweb@gmail.com>
2016-01-12 20:50:44 -0500

    Button added
    Button added

--- ArduinoPotentiometerServo/ArduinoPotentiometerServo.ino
+++ ArduinoPotentiometerServo/ArduinoPotentiometerServo.ino
@@ -2,18 +2,21 @@
 Servo test;
 int AnalogOut = 0;
 int NewAnalogOut = 0;
+int pin_Button = 12;
+int pin_Button_State;
 
 void setup() {
   Serial.begin(9600);
   test.attach(11);
+  pinMode(pin_Button, INPUT);
 }
 // test
 void loop() {
   int sensorValue = analogRead(A0);
   
   NewAnalogOut = map(sensorValue, 0, 1023, 0, 180);
-  Serial.print(AnalogOut);
-  Serial.println(NewAnalogOut);
+  pin_Button_State = digitalRead(pin_Button);
+  Serial.println(pin_Button_State);
   if (abs(NewAnalogOut - AnalogOut) > 2) {
     test.write(AnalogOut);
     AnalogOut = NewAnalogOut;
